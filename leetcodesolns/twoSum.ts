35c9dee3166f7d2c98056274927e8e0a95389a38
divineamunega <114052520+divineamunega@users.noreply.github.com>
2023-11-21 07:39:08 +0100

    Two Sum with Hashmap

--- leetcodesolns/twoSum.ts
+++ leetcodesolns/twoSum.ts
@@ -19,6 +19,8 @@
 // Output: [0,1]
 
 // Time complexity 0
+
+// Nested Loop Method
 const twoSum = function (nums: number[], target: number): number[] {
 	const arr: number[] = [];
 	let loopCounter = 0;
@@ -44,18 +46,37 @@ const twoSum = function (nums: number[], target: number): number[] {
 
 	return arr;
 };
-twoSum([1, 1, 2], 3);
-twoSum([1, 1, 2, 3], 5);
-twoSum([1, 1, 2, 3, 4], 7);
-twoSum([1, 1, 2, 3, 4, 5], 9);
-twoSum([1, 1, 2, 3, 4, 5, 6], 11);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7], 13);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8], 15);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9], 17);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 19);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 21);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 23);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 25);
-twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 27);
+// twoSum([1, 1, 2], 3);
+// twoSum([1, 1, 2, 3], 5);
+// twoSum([1, 1, 2, 3, 4], 7);
+// twoSum([1, 1, 2, 3, 4, 5], 9);
+// twoSum([1, 1, 2, 3, 4, 5, 6], 11);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7], 13);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9], 17);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 19);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 21);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 23);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 25);
+// twoSum([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 27);
+
+// HashMap Method
+// Time Complexity 0(n)
+const twoSumHash = function (nums: number[], target: number): number[] {
+	const map: Map<number, number> = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		console.log(i);
+
+		const complement = target - nums[i];
+		if (map.has(complement)) {
+			console.log([map.get(complement), i]);
+			return [map.get(complement)!, i];
+		}
+
+		map.set(nums[i], i);
+	}
+	return [0];
+};
+
+twoSumHash([-5, 1, 2, 3, 4, 5, 6, 7, 8], 15);
 
 //-----[2, 7, 11, 15]
